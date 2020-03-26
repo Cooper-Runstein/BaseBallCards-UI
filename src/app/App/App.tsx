f700c2c60a40a93db6de138023cedd5a93db5694
Cooper Runstein <cooper.runstein@vacasa.com>
2020-03-26 15:08:26 -0700

    Restructure directories

---
+++ src/app/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Baseball Card App</h2>
+      </header>
+    </div>
+  );
+}
